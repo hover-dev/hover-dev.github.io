6980dbe82019174c0f457637895fae4c42e10ec4
JScott <jvscott@gmail.com>
2013-11-03 11:23:18 -0800

    Map back-end
    The map is now persistent with a proper data structure behind it.

--- explore/game.js
+++ explore/game.js
@@ -1,3 +1,21 @@
+/**
+* Psuedo classes because I'm too lazy to strictly define them:
+*
+* Cards:
+*   - Name
+*   - Header
+*   - Text (array of paragraphs)
+*   - take (fn)
+*   - use (fn)
+*   - Class (css)
+*
+* Tiles:
+*   - Class (css)
+*
+* Will probably want to refactor these as time goes on.
+* Maybe even bring them into proper classes and instances?
+*/
+
 function GameCntl($scope) {
 	function rand(low, high) {
 		return Math.floor((Math.random()*high)+low);
@@ -39,7 +57,9 @@ function GameCntl($scope) {
 		knowledge: 2,
 		speed: 2,
 		courage: 2,
-		hand: [OCEAN_ITEMS[rand(0,2)],OCEAN_ITEMS[rand(0,2)],OCEAN_ITEMS[rand(0,2)],OCEAN_ITEMS[rand(0,2)],OCEAN_ITEMS[rand(0,2)],OCEAN_ITEMS[rand(0,2)]]
+		hand: [OCEAN_ITEMS[0], OCEAN_ITEMS[1]],
+		board: 0,
+		position: {x: 0, y: 0}
 	};
 	$scope.pickTrait = function(name) {
 		$scope.character[name] += 1;
@@ -47,16 +67,30 @@ function GameCntl($scope) {
 		$scope.drawItem();
 	};
 
-	$scope.tiles = [];
-	for (var i = 0; i < 9; i++) {
-		$scope.tiles.push({
-			name: i,
-			class: "tile"+rand(1,3)
-		});
-	}
-
 	// Set the theme
 	$scope.theme = THEMES[rand(0,THEMES.length)];
+
+	// Initial map data
+	// TODO: make start tiles depend on theme
+	$scope.map = [{ // ocean
+		"0,0": {
+			class: "tile1"//"start"
+		},
+		"1,0": {
+			class: "tile2"//"buoy"
+		},
+		"2,0": {
+			class: "shore"
+		}
+	},
+	{ // shore
+		"0,0": {
+			class: "dock"
+		}
+	}];
+	UNEXPLORED_TILE = {
+		class: "tile3"//"undiscovered"
+	}
 
 	// Phobias and disabilities that help define our character
 	// TODO: rethink this mechanics
@@ -79,4 +113,28 @@ function GameCntl($scope) {
 		var items = $scope.theme.items;
 		$scope.showCard(items[rand(0,items.length)]);
 	};
+
+	// Redraw tiles on a given coordinate
+	$scope.redrawTiles = function() {
+		// Clear the tiles
+		$scope.tiles = [];
+		// Redraw it based on the map and character position
+		var character = $scope.character;
+		for (var ymod = 1; ymod > -2; ymod--) {
+			for (var xmod = -1; xmod < 2; xmod++) {
+				// Grab character position and board
+				var pos = (character.position.x+xmod)+","+(character.position.y+ymod);
+				var map = $scope.map[character.board];
+
+				// Update the corresponding tile
+				if (pos in map) {
+					$scope.tiles.push(map[pos]);
+				}
+				else {
+					$scope.tiles.push(UNEXPLORED_TILE);
+				}
+			}
+		}
+	}
+	$scope.redrawTiles();
 }
